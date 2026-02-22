18fddda6b048096f22bc3a63fd1712fa9a3fcbe3
ClassSeats CI <ci@classseats.app>
2026-02-22 00:51:44 +0000

    Deploy MOBILE (TEST) from 7136a1f18ea459a4c491dd3374016e68da9e4b69

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const BUILD_REV = '9ca9aea99f88f54ef53960d15b521e2c9bc36bea'
+const BUILD_REV = '7136a1f18ea459a4c491dd3374016e68da9e4b69'
 const CACHE_NAME = `classseats-pwa-${BUILD_REV}`
 const CORE_ASSETS = [
   '/',
